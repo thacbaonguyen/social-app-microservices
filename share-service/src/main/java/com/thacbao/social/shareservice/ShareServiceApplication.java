ed25aa527ba2d63f447061a8bd99cce75e6df5ab
thacbaonguyen <ar.thbaonguyen@gmail.com>
2024-04-28 10:26:30 +0700

    generate share-service

---
+++ share-service/src/main/java/com/thacbao/social/shareservice/ShareServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thacbao.social.shareservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareServiceApplication.class, args);
+	}
+
+}
---
+++ share-service/src/test/java/com/thacbao/social/shareservice/ShareServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thacbao.social.shareservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
